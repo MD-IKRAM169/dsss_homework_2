3aa5dd5ca66fd6869923acf76bd4a7c8ef43a356
Md Ikram Tareq <61970296+MD-IKRAM169@users.noreply.github.com>
2024-11-09 14:29:58 +0100

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='dsss_homework_2',
+    version='0.1',
+    description='Data Science Survival Skill',
+    author='Ikram',
+    packages=find_packages(exclude=['testt']),
+    zip_safe=False
+)
